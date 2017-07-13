ffbc501c2fcc9b8815078c2777d86a7668a088eb
Kosal Vann <kosal.vann@gmail.com>
2017-07-12 23:10:52 -0500

    Test three more methods and added docblock

--- app/Cart.php
+++ app/Cart.php
@@ -3,17 +3,75 @@
 namespace App;
 
 /**
- * Add item to the cart
+ * Magement of the item in the shopping cart
  */
 class Cart {
+    /**
+     * Declare properties
+     *
+     * @var array $items A list of product items
+     */
     private $items = [];
 
+    /**
+     * @var str $item An item within the cart
+     */
+    private $item;
+
+    /**
+     * @var int $totalItemCount The number of item in the cart
+     */
+    private $totalItemCount;
+
+    /**
+     * Initialize our properties
+     *
+     * @param array $items
+     */
+    public function __construct($items = [])
+    {
+        $this->items = $items;
+    }
+
+    /**
+     * Add an item to the cart
+     *
+     * @param str $item The item/product being added to the cart
+     * @return void
+     */
     public function addItem($item)
     {
+        // Push item into list of array
         array_push($this->items, $item);
     }
+
+    /**
+     * Remove an item from the cart
+     *
+     * @return void
+     */
+    public function removeItem($item)
+    {
+        return false;
+    }
+
+    /**
+     * Get list of items as an array
+     *
+     * @return array Item[]
+     */
     public function getItems()
     {
         return $this->items;
+    }
+
+    /**
+     * Get the count of items in the cart as a number
+     *
+     * @return int
+     */
+    public function getItemsCount()
+    {
+        return $this->totalItemCount;
     }
 }
--- tests/Unit/CartTest.php
+++ tests/Unit/CartTest.php
@@ -4,18 +4,52 @@ use App\Cart;
 
 class CartTest extends TestCase
 {
+    /**
+     * If item is added to the cart
+     *
+     * @covers Cart::getItems
+     */
     public function testAddItem()
     {
         $cart = new Cart();
-
         $cart->addItem('foo');
         $this->assertEquals(array('foo'), $cart->getItems());
         $this->assertNotEquals(array('bar'), $cart->getItems());
     }
+
+    /**
+     * Remove an item from the cart
+     *
+     * @covers Cart::getItems
+     */
+    public function testRemoveItem()
+    {
+        $cart = new Cart();
+        $cart->removeItem('foo');
+        $this->assertEmpty('', $cart->getItems());
+    }
+
+    /**
+     * Get the list of items in the cart
+     *
+     * @covers Cart::getItems
+     */
     public function testGetItems()
     {
         $cart = new Cart();
-
         $this->assertEquals(array(), $cart->getItems());
+    }
+
+    /**
+     * Get the list of items, as a number, in the cart
+     *
+     * @covers Cart::getItems
+     * @depends testGetItems
+     */
+    public function testGetItemsCount()
+    {
+        $cart = new Cart(array(1, 2, 3));
+        $cart->getItems();
+        $this->assertEquals(3, count($cart->getItems()));
     }
 }
